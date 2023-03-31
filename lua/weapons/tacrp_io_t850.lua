6f68d327a4d0596022211d8da3ec886a639d97da
TheOnly8Z <888888zombies@gmail.com>
2023-03-30 23:40:59 -0500

    taurus 850

---
+++ lua/weapons/tacrp_io_t850.lua
@@ -0,0 +1,230 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "Taurus 850 Ultralite"
+SWEP.Category = "Tactical RP" // "Tactical RP (Arctic)"
+
+SWEP.SubCatTier = "4Common"
+SWEP.SubCatType = "1Sidearm"
+
+SWEP.Description = "Snub-nosed revolver with compact form factor. High mobility and hip fire accuracy, and good recoil among magnum pistols."
+
+SWEP.ViewModel = "models/weapons/tacint_extras/v_t850.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_mr96.mdl"
+
+SWEP.Slot = 1
+
+SWEP.ArcadeStats = {
+    MoveSpeedMult = 1,
+    ShootingSpeedMult = 0.85,
+    SightedSpeedMult = 1,
+    MeleeSpeedMult = 1,
+    ReloadSpeedMult = 1,
+}
+
+// "ballistics"
+
+SWEP.Damage_Max = 50 // damage at minimum range
+SWEP.Damage_Min = 18 // damage at maximum range
+SWEP.Range_Min = 600 // distance for which to maintain maximum damage
+SWEP.Range_Max = 2500 // distance at which we drop to minimum damage
+SWEP.Penetration = 6 // units of metal this weapon can penetrate
+
+SWEP.MuzzleVelocity = 13000
+
+SWEP.BodyDamageMultipliers = {
+    [HITGROUP_HEAD] = 2.5,
+    [HITGROUP_CHEST] = 1.25,
+    [HITGROUP_STOMACH] = 1,
+    [HITGROUP_LEFTARM] = 0.9,
+    [HITGROUP_RIGHTARM] = 0.9,
+    [HITGROUP_LEFTLEG] = 0.75,
+    [HITGROUP_RIGHTLEG] = 0.75,
+    [HITGROUP_GEAR] = 0.75
+}
+
+// misc. shooting
+
+SWEP.Firemode = 1
+
+SWEP.FiremodeName = "Double-Action" // only used externally for firemode name distinction
+
+SWEP.RPM = 200
+
+SWEP.Spread = 0.008
+
+SWEP.ShootTimeMult = 1
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 3
+SWEP.RecoilResetTime = 0.25
+SWEP.RecoilDissipationRate = 7
+SWEP.RecoilFirstShotMult = 0.8
+
+SWEP.RecoilVisualKick = 4
+
+SWEP.RecoilKick = 10
+
+SWEP.RecoilSpreadPenalty = 0.01
+SWEP.HipFireSpreadPenalty = 0.005
+
+SWEP.CanBlindFire = true
+
+// handling
+
+
+SWEP.MoveSpeedMult = 0.9
+SWEP.ShootingSpeedMult = 0.7
+SWEP.SightedSpeedMult = 0.5
+
+SWEP.ReloadSpeedMult = 0.75
+
+SWEP.AimDownSightsTime = 0.22
+SWEP.SprintToFireTime = 0.22
+
+// hold types
+
+SWEP.HoldType = "revolver"
+SWEP.HoldTypeSprint = "normal"
+SWEP.HoldTypeBlindFire = "pistol"
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_REVOLVER
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_REVOLVER
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -4)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(3, -2, -5)
+
+SWEP.SprintAng = Angle(0, 30, 0)
+SWEP.SprintPos = Vector(2, 0, -12)
+
+SWEP.SightAng = Angle(-0.05, -0.2, 0.5)
+SWEP.SightPos = Vector(-3.475, 0, -2.5)
+
+SWEP.HolsterVisible = true
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_PISTOL
+SWEP.HolsterPos = Vector(0, 3, -4)
+SWEP.HolsterAng = Angle(90, 0, 0)
+
+SWEP.Sway = 2
+SWEP.ScopedSway = 1
+
+// reload
+
+SWEP.ClipSize = 5
+SWEP.Ammo = "357"
+
+SWEP.ReloadTimeMult = 0.9
+
+// sounds
+
+local path = "tacint_extras/t850/"
+local path1 = "tacrp/weapons/mr96/"
+SWEP.Sound_Shoot = "^" .. path .. "deagle-1.wav"
+
+SWEP.Vol_Shoot = 130
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 2
+SWEP.QCA_Eject = 0
+SWEP.EjectEffect = 0
+
+SWEP.MuzzleEffect = "muzzleflash_1"
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["deploy"] = "draw",
+    ["fire"] = {"shoot1", "shoot2", "shoot3"},
+    ["fire_iron"] = "shoot1",
+    ["blind_fire"] = {"blind_shoot1", "blind_shoot2"},
+    ["melee"] = {"melee1", "melee2"},
+    ["jam"] = "draw",
+}
+
+// attachments
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Optic",
+        Category = "optic_pistol",
+        Bone = "ValveBiped.mr96_rootbone",
+        WMBone = "Box01",
+        AttachSound = "TacRP/weapons/optic_on.wav",
+        DetachSound = "TacRP/weapons/optic_off.wav",
+        VMScale = 1,
+        WMScale = 1,
+        Pos_VM = Vector(-3, -0.1, 3),
+        Ang_VM = Angle(90, 0, 0),
+        Pos_WM = Vector(0, 1.5, -0.8),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [2] = {
+        PrintName = "Accessory",
+        Category = {"acc", "acc_holster", "acc_brace"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [3] = {
+        PrintName = "Trigger",
+        Category = {"trigger_revolver"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [4] = {
+        PrintName = "Ammo",
+        Category = {"ammo_pistol", "ammo_roulette"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [5] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_extras_t850.Release_Cylinder", path1 .. "mr96_release_cylinder.wav")
+addsound("tacint_extras_t850.Eject_Shells", path1 .. "mr96_eject_shells.wav")
+addsound("tacint_extras_t850.Insert_Bullets", path1 .. "mr96_insert_bullets.wav")
+addsound("tacint_extras_t850.Shut_Cylinder", path1 .. "mr96_shut_cylinder.wav")
+addsound("tacint_extras_t850.Insert_Bullets-Mid", path1 .. "mr96_insert_bullets-mid.wav")
+addsound("tacint_extras_t850.Cock_Hammer", path1 .. "mr96_cockhammer.wav")
+addsound("tacint_extras_t850.Deploy", "weapons/357/357_spin1.wav")
