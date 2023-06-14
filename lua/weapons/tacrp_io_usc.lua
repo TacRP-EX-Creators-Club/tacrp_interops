9543694d7f5c44d98ebae2cef425ffa355818aba
TheOnly8Z <888888zombies@gmail.com>
2023-06-14 02:31:14 -0500

    usc ext +3 instead of +5

--- lua/weapons/tacrp_io_usc.lua
+++ lua/weapons/tacrp_io_usc.lua
@@ -268,7 +268,7 @@ SWEP.Attachments = {
     },
     [4] = {
         PrintName = "Accessory",
-        Category = {"acc", "acc_sling", "acc_duffle", "acc_extmag_smg"},
+        Category = {"acc", "acc_sling", "acc_duffle", "acc_extmag_pistol"},
         AttachSound = "tacrp/weapons/flashlight_on.wav",
         DetachSound = "tacrp/weapons/flashlight_off.wav",
     },
