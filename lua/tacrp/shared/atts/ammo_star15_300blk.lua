28d6ce14d5e4ceae5f32495edb9e71585d13459c
TheOnly8Z <888888zombies@gmail.com>
2023-07-23 14:18:55 -0500

    attachment localization

--- lua/tacrp/shared/atts/ammo_star15_300blk.lua
+++ lua/tacrp/shared/atts/ammo_star15_300blk.lua
@@ -2,8 +2,8 @@ ATT.PrintName = ".300 BLK"
 ATT.FullName = "ST AR-15 .300 AAC Blackout Mod Kit"
 ATT.Icon = Material("entities/tacrp_att_ammo_star15_300blk.png", "mips smooth")
 ATT.Description = "Modification to load lower velocity bullets with better CQB potential."
-ATT.Pros = {"Close Range Damage"}
-ATT.Cons = {"Spread", "Muzzle Velocity, Range", "Penetration"}
+ATT.Pros = {"stat.damage_max"}
+ATT.Cons = {"rating.range", "stat.muzzlevelocity", "att.procon.armor"}
 
 ATT.Category = "ammo_star15"
 ATT.SortOrder = 0
@@ -19,6 +19,7 @@ ATT.Mult_RecoilSpreadPenalty = 1.1
 
 ATT.Mult_Penetration = 0.75
 ATT.Add_ArmorPenetration = -0.15
+ATT.Mult_ArmorBonus = 0.5
 
 -- forgive me for fudging the dist sounds
 ATT.Override_Sound_Shoot = "^tacint_extras/star15/fire-03.ogg"
--- lua/tacrp/shared/atts/ammo_star15_50beo.lua
+++ lua/tacrp/shared/atts/ammo_star15_50beo.lua
@@ -2,8 +2,8 @@ ATT.PrintName = ".50 BEO"
 ATT.FullName = "ST AR-15 .50 Beowulf Mod Kit"
 ATT.Icon = Material("entities/tacrp_att_ammo_star15_50beo.png", "mips smooth")
 ATT.Description = "Modification to load low capacity, high power magnum rounds."
-ATT.Pros = {"Damage increased significantly"}
-ATT.Cons = {"Capacity, Fire Rate, Recoil", "Range, Muzzle Velocity", "Penetration"}
+ATT.Pros = {"rating.lethality"}
+ATT.Cons = {"rating.range", "rating.control", "att.procon.armor"}
 
 ATT.Category = "ammo_star15"
 ATT.SortOrder = 0.5
@@ -14,10 +14,10 @@ ATT.Mult_Damage_Max = 2
 ATT.Mult_Damage_Min = 1.5
 ATT.Mult_Range_Min = 0.75
 ATT.Mult_Range_Max = 0.5
-ATT.Add_ArmorPenetration = -0.1
-ATT.Mult_ArmorBonus = 0.5
 
 ATT.Mult_Penetration = 0.25
+ATT.Add_ArmorPenetration = -0.1
+ATT.Mult_ArmorBonus = 0.75
 
 ATT.Mult_RPM = 0.55
 ATT.Mult_ClipSize = 1 / 3
