0e1ab4af4773437dd427117a630b415dea692138
TheOnly8Z <888888zombies@gmail.com>
2023-08-01 18:15:41 -0500

    remove boilerplate comments

--- lua/weapons/tacrp_io_m16a2.lua
+++ lua/weapons/tacrp_io_m16a2.lua
@@ -5,7 +5,7 @@ AddCSLuaFile()
 
 // names and stuff
 SWEP.PrintName = "Colt M16A2"
-SWEP.Category = "Tactical RP" // "Tactical RP (Arctic)"
+SWEP.Category = "Tactical RP"
 
 SWEP.SubCatTier = "3Security"
 SWEP.SubCatType = "4Assault Rifle"
@@ -205,25 +205,6 @@ SWEP.EjectEffect = 2
 
 // anims
 
-// VM:
-// idle
-// fire
-// fire1, fire2...
-// dryfire
-// melee
-// reload
-// midreload
-// prime_grenade
-// throw_grenade
-// throw_grenade_underhand
-// deploy
-// blind_idle
-// blind_fire
-// blind_fire1, blind_fire2...
-// blind_dryfire
-
-// WM:
-// attack1
 SWEP.AnimationTranslationTable = {
     ["fire_iron"] = "dryfire",
     ["fire1"] = "fire1_M",
