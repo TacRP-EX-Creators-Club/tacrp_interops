5d496ef96daf486a862c770f799f4b539e55824b
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-11-13 13:32:41 -0500

    god bless americunt

--- lua/weapons/tacrp_io_degala.lua
+++ lua/weapons/tacrp_io_degala.lua
@@ -14,7 +14,7 @@ SWEP.Description = "Imposing magnum pistol, as iconic as it gets.\nPowerful and 
 SWEP.Description_Quote = "\"one deag, two deag, three deag, four deag...\""
 
 SWEP.Trivia_Caliber = ".50 AE"
-SWEP.Trivia_Manufacturer = "Israel Military Industries"
+SWEP.Trivia_Manufacturer = "Magnum Research"
 SWEP.Trivia_Year = "1995" // It's a Mark XIX.  Teardrop safety lever, top scope rail and taller cocking serrations on the slide (yes i am severely autistic).
 
 SWEP.Faction = TacRP.FACTION_NEUTRAL
